49b3d6a06f966cc2a3dafac4a523dcf45049bcbe
EKennard <lizz.kennard@hotmail.com>
2025-06-12 22:25:13 +0100

    script started for rock papaer scissors

---
+++ rock-paper-scissors.js
@@ -0,0 +1,10 @@
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors")
+const results = document.getElementById("game-result");
+const resetBtn = document.getElementById("reset");
+const winningCombinations = [[0, 2], [1, 0], [2, 1]];
+
+// need to set these to write not get
+const playerChoice = document.getElelemtById (player-choice);
+const computerChoice = document.getElementById (computer-choice)
