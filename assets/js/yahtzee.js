ddb46ebd4337a36aeabb8a26f5e94640b24337b3
EKennard <lizz.kennard@hotmail.com>
2025-06-17 16:55:10 +0100

    yahtzee game page and navs added

---
+++ assets/js/yahtzee.js
@@ -0,0 +1,7 @@
+// Unicodes for Dice
+// - ⚀ (U+2680) → Dice Face 1
+// - ⚁ (U+2681) → Dice Face 2
+// - ⚂ (U+2682) → Dice Face 3
+// - ⚃ (U+2683) → Dice Face 4
+// - ⚄ (U+2684) → Dice Face 5
+// - ⚅ (U+2685) → Dice Face 6
